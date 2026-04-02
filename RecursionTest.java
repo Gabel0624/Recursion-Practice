57a64b66adaf977f5ac59c8fa3bb5e71d1c8bbbe
Grayson Abel <graysonabel0624@gmail.com>
2026-04-02 12:43:25 -0400

    Initial commit

---
+++ RecursionTest.java
@@ -0,0 +1,15 @@
+/**
+ * @author Grayson Abel
+ */
+public class RecursionTest {
+    public static String reverse (String str){
+        if (str.isEmpty()) {
+            return str;
+        }
+        return reverse (str.substring(1)) + str.charAt(0);
+    }
+    public static void main(String[] args) {
+        System.out.println(reverse ("Hello"));
+
+    }
+}
